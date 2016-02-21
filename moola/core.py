9d397f586779efbc706b5d0cf4f3687454c73e16
Pete Graham <git@petegraham.co.uk>
2016-02-21 15:40:11 -0400

    Split logic into two for loops

--- moola/core.py
+++ moola/core.py
@@ -9,19 +9,26 @@ def calc_daily_balances_for_month(year, month, start_balance, end_balance):
     end_balance = end_balance * 100
 
     days_range = get_day_range_for_month(year, month)
+    num_days = len(days_range)
     monthly_spend = start_balance - end_balance
-    daily_spend = calc_daily_spending_amount(monthly_spend, len(days_range))
+    daily_spend = calc_daily_spending_amount(monthly_spend, num_days)
 
-    daily_balances = []
-    Balance = namedtuple('Balance', 'date balance')
+    # breakout into a function
+    balances = []
     daily_balance = start_balance
-
-    for day in days_range:
-        date = datetime(year, month, day)
-        # Round and convert back to pounds here
-        date_balance = Balance(date, int(daily_balance) / 100)
-        daily_balances.append(date_balance)
+    for day in range(num_days):
+        balances.append(daily_balance)
         daily_balance -= daily_spend
+
+    # breakout into a function, or list comp
+    Balance = namedtuple('Balance', 'date balance')
+    daily_balances = []
+    for day, balance in enumerate(balances):
+        date = datetime(year, month, day + 1)
+        # Round and convert back to pounds here
+        date_balance = Balance(date, int(balance) / 100)
+        daily_balances.append(date_balance)
+
     return daily_balances
 
 
