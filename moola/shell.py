8bff7622badcd19d1b5b29cca69439140e7120dc
Pete Graham <git@petegraham.co.uk>
2016-02-21 14:28:59 -0400

    Use named tuple to store daily balances

--- moola/shell.py
+++ moola/shell.py
@@ -1,5 +1,6 @@
 from calendar import monthrange
 from datetime import datetime
+from collections import namedtuple
 
 
 def create_sheet_for_month():
@@ -10,11 +11,15 @@ def create_sheet_for_month():
     pass
 
 
+# TOOD: rename get daily_balances for month
 def get_amounts_for_month(year, month, start_balance, end_balance):
-    amounts = []
+    daily_balances = []
+    Balance = namedtuple('Balance', 'date amount')
+    # TODO: break into function
     num_days = monthrange(year, month)[1]
+
     for day in range(1, num_days + 1):
         date = datetime(year, month, day)
-        # TOOD: use named tuple
-        amounts.append((date, 2500))
-    return amounts
+        date_balance = Balance(date, 2500)
+        daily_balances.append(date_balance)
+    return daily_balances
