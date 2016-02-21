902ed0029cc03851bd72b33d0893ee16f08ec069
Pete Graham <git@petegraham.co.uk>
2016-02-21 14:18:36 -0400

    Get amounts returns dates and amounts

--- moola/shell.py
+++ moola/shell.py
@@ -1,4 +1,5 @@
 from calendar import monthrange
+from datetime import datetime
 
 
 def create_sheet_for_month():
@@ -9,6 +10,11 @@ def create_sheet_for_month():
     pass
 
 
-def get_amounts_for_month(year, month):
+def get_amounts_for_month(year, month, start_balance, end_balance):
+    amounts = []
     num_days = monthrange(year, month)[1]
-    return list(range(num_days))
+    for day in range(1, num_days + 1):
+        date = datetime(year, month, day)
+        # TOOD: use named tuple
+        amounts.append((date, 2500))
+    return amounts
--- tests/test_shell.py
+++ tests/test_shell.py
@@ -1,11 +1,48 @@
+from datetime import datetime
+
 from moola.shell import get_amounts_for_month
 
 
 def test_get_amounts_for_month_amount_for_each_day():
-    amounts = get_amounts_for_month(year=2016, month=1)
+    amounts = get_amounts_for_month(
+        year=2016,
+        month=1,
+        start_balance=2500,
+        end_balance=500)
     assert len(amounts) == 31
 
 
 def test_get_amounts_for_month_amount_for_each_day_leap_year():
-    amounts = get_amounts_for_month(year=2016, month=2)
+    amounts = get_amounts_for_month(
+        year=2016,
+        month=2,
+        start_balance=2500,
+        end_balance=500)
     assert len(amounts) == 29
+
+
+def test_get_amounts_for_month_first_item_correct_date():
+    amounts = get_amounts_for_month(
+        year=2016,
+        month=2,
+        start_balance=2500,
+        end_balance=500)
+    assert amounts[0][0] == datetime(2016, 2, 1)
+
+
+def test_get_amounts_for_month_last_item_correct_date():
+    amounts = get_amounts_for_month(
+        year=2016,
+        month=2,
+        start_balance=2500,
+        end_balance=500)
+    assert amounts[-1][0] == datetime(2016, 2, 29)
+
+
+def test_get_amounts_for_month_first_item_correct_balance():
+    amounts = get_amounts_for_month(
+        year=2016,
+        month=2,
+        start_balance=2500,
+        end_balance=500)
+    assert amounts[0][1] == 2500
