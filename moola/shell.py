9cfa58cc1ad02af87ba961e9b21c0a4bfc473257
Pete Graham <git@petegraham.co.uk>
2016-02-21 14:07:09 -0400

    Get correct number of days for month

--- moola/shell.py
+++ moola/shell.py
@@ -1,3 +1,6 @@
+from calendar import monthrange
+
+
 def create_sheet_for_month():
     """
     Prompt user for required values then create Google spreadsheet with amounts
@@ -6,5 +9,6 @@ def create_sheet_for_month():
     pass
 
 
-def get_amounts_for_month(month, year):
-    return list(range(29))
+def get_amounts_for_month(year, month):
+    num_days = monthrange(year, month)[1]
+    return list(range(num_days))
--- tests/test_shell.py
+++ tests/test_shell.py
@@ -2,10 +2,10 @@ from moola.shell import get_amounts_for_month
 
 
 def test_get_amounts_for_month_amount_for_each_day():
-    amounts = get_amounts_for_month(month=2, year=2017)
+    amounts = get_amounts_for_month(year=2016, month=1)
+    assert len(amounts) == 31
+
+
+def test_get_amounts_for_month_amount_for_each_day_leap_year():
+    amounts = get_amounts_for_month(year=2016, month=2)
     assert len(amounts) == 29
-
-
-# def test_get_amounts_for_month_amount_for_each_day_leap_year():
-#     amounts = get_amounts_for_month(month=1, year=2017)
-#     assert len(amounts) == 31
