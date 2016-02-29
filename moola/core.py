7073717e2947773e71011ebb011b30c708fc0bdd
Pete Graham <git@petegraham.co.uk>
2016-02-29 17:03:23 +0000

    Core functionality for calculating transactions

--- moola/core.py
+++ moola/core.py
@@ -3,41 +3,78 @@ from datetime import datetime
 from collections import namedtuple
 
 
-def calc_daily_balances_for_month(year, month, start_balance, end_balance):
+def calc_daily_balances_for_month(
+        year, month, start_balance, end_balance, transactions=None):
     """
     Wrapper function which converts amounts to pence, calls the actual logic
     then formats the return
     """
     balances = _calc_daily_balances_for_month(
-        year, month, start_balance * 100, end_balance * 100)
+        year,
+        month,
+        start_balance * 100,
+        end_balance * 100,
+        convert_transactions_pence(transactions))
+
     formatted_balances = []
     for index, balance in enumerate(balances):
         formatted_balances.append(format_balance(year, month, index, balance))
     return formatted_balances
 
 
-def _calc_daily_balances_for_month(year, month, start_balance, end_balance):
+def _calc_daily_balances_for_month(
+        year, month, start_balance, end_balance, transactions=None):
     num_days = get_number_of_days_in_month(year, month)
-    monthly_spend = start_balance - end_balance
+    transactions_total = get_transactions_total(transactions)
+    monthly_spend = start_balance - end_balance + transactions_total
     daily_spend = calc_daily_spending_amount(monthly_spend, num_days)
 
     balances = []
-    for day in range(num_days):
-        balances.append(calc_balance(day, daily_spend, start_balance))
+    for index in range(num_days):
+        day = index + 1
+        transaction_amount = calc_transactions_up_to_day(day, transactions)
+        balance = calc_balance(
+            index,
+            daily_spend,
+            start_balance,
+            transaction_amount)
+        balances.append(balance)
     return balances
+
+
+# TODO: needs unit tests
+def convert_transactions_pence(transactions):
+    Transaction = namedtuple('Transaction', 'day amount description')
+    if transactions is None:
+        return []
+    return [
+        Transaction(t.day, t.amount * 100, t.description)for t in transactions]
+
+
+def calc_transactions_up_to_day(day, transactions):
+    """
+    Calculate transactions up to the current day of the month
+    """
+    total = 0
+    for transaction in transactions:
+        if transaction.day <= day:
+            total += transaction.amount
+    return total
 
 
 def get_transactions_total(transactions):
     total = 0
     for transaction in transactions:
-        total += transaction.amount * 100
+        total += transaction.amount
     return total
 
 
-def calc_balance(index, daily_spend, start_balance):
-    return start_balance - (daily_spend * index)
+# TODO: needs unit test
+def calc_balance(index, daily_spend, start_balance, transaction_amount):
+    return start_balance - (daily_spend * index) + transaction_amount
 
 
+# TODO: needs unit test
 def format_balance(year, month, index, balance):
     """
     Return balance as a tuple containg the date and the amount in pounds
@@ -51,5 +88,6 @@ def calc_daily_spending_amount(monthly_spend_pence, num_days):
     return monthly_spend_pence / num_days
 
 
+# TODO: needs unit test
 def get_number_of_days_in_month(year, month):
     return monthrange(year, month)[1]
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,13 @@
+import pytest
+
+from collections import namedtuple
 from datetime import datetime
 
 from moola.core import (
     calc_daily_balances_for_month,
-    calc_daily_spending_amount)
+    calc_daily_spending_amount,
+    calc_transactions_up_to_day,
+    get_transactions_total)
 
 
 def test_calc_daily_balances_for_month_amount_for_each_day():
@@ -72,17 +77,77 @@ def test_calc_daily_spending_amount():
     assert calc_daily_spending_amount(200000, 29) == 6896.551724137931
 
 
-# def test_calc_daily_balances_correct_balance_with_transaction():
-#     from collections import namedtuple
-#     # TODO: pull named Tuple from actual code
-#     Transaction = namedtuple('Transaction', 'day amount description')
-#     transactions = Transaction(2, 9.99, 'Nexflix')
+def test_calc_daily_balances_correct_balance_with_one_transaction():
+    # TODO: pull named Tuple from actual code
+    # TODO: use class instead of named tuple
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [Transaction(2, -9.99, 'Nexflix')]
 
-#     amounts = calc_daily_balances_for_month(
-#         year=2016,
-#         month=2,
-#         start_balance=2500,
-#         end_balance=500,
-#         transactions=transactions)
-#     daily_amount = 50
-#     assert amounts[1].balance == 2500 - daily_amount - 9.99
+    amounts = calc_daily_balances_for_month(
+        year=2016,
+        month=2,
+        start_balance=2909.99,  # 100 spend a day
+        end_balance=0,
+        transactions=transactions)
+    assert amounts[1].balance == 2800
+
+
+def test_calc_daily_balances_correct_balance_with_two_transactions():
+    # TODO: pull named Tuple from actual code
+    # TODO: use class instead of named tuple
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [
+        Transaction(1, -5.00, 'Spotify'),
+        Transaction(1, -9.99, 'Nexflix')]
+
+    amounts = calc_daily_balances_for_month(
+        year=2016,
+        month=2,
+        start_balance=2914.99,  # 100 spend a day
+        end_balance=0,
+        transactions=transactions)
+    assert amounts[1].balance == 2800
+
+
+def test_get_transactions_total_no_transactions():
+    assert get_transactions_total([]) == 0
+
+
+def test_get_transactions_total_one_transaction():
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [Transaction(2, -9.99, 'Nexflix')]
+    assert get_transactions_total(transactions) == -9.99
+
+
+def test_get_transactions_total_two_transactions():
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [
+        Transaction(2, -9.99, 'Nexflix'),
+        Transaction(2, -5.00, 'Spotify')]
+    assert get_transactions_total(transactions) == -14.99
+
+
+def test_calc_transactions_up_to_day_no_transactions():
+    assert calc_transactions_up_to_day(2, []) == 0
+
+
+def test_calc_transactions_up_to_day_one_transaction():
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [Transaction(2, -9.99, 'Nexflix')]
+    assert calc_transactions_up_to_day(2, transactions) == -9.99
+
+
+def test_calc_transactions_up_to_day_two_transactions_different_days():
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [
+        Transaction(1, -9.99, 'Nexflix'),
+        Transaction(2, -5.00, 'Spotify')]
+    assert calc_transactions_up_to_day(2, transactions) == -14.99
+
+
+def test_calc_transactions_up_to_day_transaction_after_day():
+    Transaction = namedtuple('Transaction', 'day amount description')
+    transactions = [
+        Transaction(2, -9.99, 'Nexflix'),
+        Transaction(3, -5.00, 'Spotify')]
+    assert calc_transactions_up_to_day(2, transactions) == -9.99
