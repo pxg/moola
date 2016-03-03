ef5ed64e619991549c9e619e85e4f626b6ded0d0
Pete Graham <git@petegraham.co.uk>
2016-03-03 18:06:44 +0000

    Moved fixtures to conftest.py

--- tests/shell/test_get_montly_transactions.py
+++ tests/shell/test_get_montly_transactions.py
@@ -1,33 +1,5 @@
-import pytest
-from mock import Mock
-
 from moola.shell import _get_monthly_transactions
 from moola.models import Transaction, Money
-
-
-@pytest.fixture
-def spreadsheet():
-    transaction_data = [
-        ['day', 'amount', 'description'],
-        [2, -9.99, 'Netflix'],
-        [3, -5.00, 'Spotfiy']]
-    worksheet = Mock()
-    worksheet.get_all_values = Mock(return_value=transaction_data)
-    spreadsheet = Mock()
-    spreadsheet.worksheet = Mock(return_value=worksheet)
-    return spreadsheet
-
-
-@pytest.fixture
-def spreadsheet_dirty_data():
-    transaction_data = [
-        ['day', 'amount', 'description'],
-        [2, '-£1,499.99', 'Rent']]
-    worksheet = Mock()
-    worksheet.get_all_values = Mock(return_value=transaction_data)
-    spreadsheet = Mock()
-    spreadsheet.worksheet = Mock(return_value=worksheet)
-    return spreadsheet
 
 
 def test_get_monthly_transactions_returns_list(spreadsheet):
